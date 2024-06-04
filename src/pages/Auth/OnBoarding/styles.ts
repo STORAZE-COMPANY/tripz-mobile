e43b995b776629e091d0eb3acbd9ec85fe10adff
SabrinaStoraze <sabrina.barbosa@storaze.com.br>
2024-06-04 14:15:14 -0300

    Add i18n in the page OnBoarding

--- src/pages/Auth/OnBoarding/styles.ts
+++ src/pages/Auth/OnBoarding/styles.ts
@@ -1,20 +1,95 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({
-    container: {
+export const styles = StyleSheet.create({
+    slide: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        bottom: 10,
+       
     },
-    title: {
-        fontSize: 24,
+    text: {
+        fontSize: 44,
         fontWeight: 'bold',
-        marginBottom: 16,
-        color: '#333',
+        marginTop: 20,
+        textAlign: 'center',
+        paddingHorizontal: 28,
     },
     subtitle: {
         fontSize: 16,
-        color: '#888',
+        marginTop: 10,
+        textAlign: 'center',
+        color: '#555',
+     
+    },
+    subtitleRemember: {
+        fontSize: 16,
+        marginTop: 10,
+        color: '#553',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        
+    },
+    textRemember: {
+        fontSize: 16,
+        marginTop: 10,
+        textAlign: 'center',
+        color: '#555',
+    },
+    footer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        width: '100%',
+    },
+    skipButton: {
+        color: '#2F419E',
+        fontSize: 16,
+    },
+    nextButton: {
+        backgroundColor: '#2F419E',
+        borderRadius: 10,
+        padding: 10,
+    },
+    indicatorContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    indicator: {
+        width: 45,
+        height: 2,
+        borderRadius: 2,
+        marginHorizontal: 5,
+    },
+    activeIndicator: {
+        backgroundColor: '#2F419E',
+    },
+    inactiveIndicator: {
+        backgroundColor: 'gray',
+    },
+    locationButton: {
+        marginTop: 20,
+        backgroundColor: '#2F419E',
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderRadius: 10,
+        alignItems: 'center',
+        width: 200,
+       
+    },
+    locationButtonText: {
+        color: 'white',
+        fontSize: 16,
+    },
+    disabledText: {
+        color: 'gray',
+    },
+    disabledNextButton: {
+        backgroundColor: 'gray',
     },
 });
 
